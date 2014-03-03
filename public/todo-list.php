4c57f343aea82e71b3db3962616552627652a803
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-03 13:43:56 -0600

    todo-list.php throwing exceptions

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,48 +1,83 @@
 
 <?php   
+require_once('classes/filestore.php');
 // Usedfor debugging purposes
 var_dump($_POST);  // This shows the contents of the method POST
 var_dump($_GET);   // This shows the contents of the method GET
-var_dump($_FILES);
+var_dump($_FILES); // Shows the contents of the method FILES
+$filename = "todo.txt";
+$error_msg = false;
 
-$filename = 'todo.txt';  // name of the file 
+class TodoList extends FileStore{
+ 
+  // Set the defaults items array
+  public $items = array();
 
-// Check if the file size is greater than 0 if true it opens the file an puts the contents into items array otherwise it creates
-//  an empty array
+ 
+  // Set list items and optional filename
+  public function __construct($filename = '') {
+    if (!empty($filename)) {
+      $this->filename = $filename;
+      $this->items = $this->get_list();
+    }
 
-$items = (filesize($filename) > 0) ? $items = open_file($filename) : array();
+    var_dump($this);
+  }
+ 
+  // Returns an array of todo items
+  public function get_list() {
+    if (filesize($this->filename) > 0) {
+      return $this->read($this->filename);
+    } else {
+      return array();
+    }
+  }
+ 
+  // Add item to list, return new list
+  public function add_item($item) {
 
-
-// Function that opens a file, reads its content and returns an array of the contents called items
-function open_file($filename){      
-  // Load files
-  $handle = fopen ($filename, 'r');
-  $contents = fread($handle, filesize($filename));
-  fclose($handle);
-  $array_contents = explode(PHP_EOL, $contents);
-  $items = $array_contents;
-  return $items;   
+    // if ((strlen($item)<= 240) && (!empty($item))){ 
+      $new_item = htmlspecialchars(strip_tags($item));
+      array_push($this->items, $new_item);
+      $this->write($this->items);
+  // }else{
+    // throw new Exception("The item you are entering is empty or you exceed 240 characters");
+  // } 
+ }
+ 
+  // Remove item from list, redirect optional
+  public function remove_item($key, $redirect = FALSE) {
+    unset($this->items[$key]);
+    $this->write($this->items);
+    if (is_string($redirect)) {
+      header("Location: $redirect");
+      exit(0);  
+    } 
+  }
+ 
+}
+ 
+// Get new instance of TodoList
+$list = new TodoList('todo.txt');
+$items = $list->read();
+ 
+// Check for removal from list - process if exists
+if (isset($_GET['remove'])) {
+  $list->remove_item($_GET['remove'], 'todo-list.php');
+}
+ 
+// Check for new item - process if exists
+if (!empty($_POST['newitem'])&& (strlen($_POST['newitem']> 240))) {
+  $list->add_item($_POST['newitem']);
+}else{
+  throw new Exception("The item you are entering is empty or you exceed 240 characters");
 }
 
 
-// Function that writes in a file a list of items 
-function save_file ($filename, $items){
-  $itemsString = implode (PHP_EOL, $items);
-  $handle = fopen ($filename, 'w');
-  // Save to file 
-  fwrite($handle, $itemsString);
-  fclose($handle);
-}
+if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {    
 
-// Variable that will be used to echo a msg when a file is not a text file
-$error_msg = '';
-if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
+     if ($_FILES['upload_file']['type'] == 'text/plain'){
 
-     if($_FILES['upload_file']['type'] != 'text/plain'){
-         
-         $error_msg = true;
-
-     }else{
         // Set the destination directory for uploads
         $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
         // Grab the filename from the uploaded file by using basename
@@ -51,71 +86,61 @@ if (count($_FILES) > 0 && $_FILES['upload_file']['error'] == 0) {
         $saved_filename = $upload_dir . $newfilename;
         // Move the file from the temp location to our uploads directory
         move_uploaded_file($_FILES['upload_file']['tmp_name'], $saved_filename); 
-    
-        // Load $ array with file contents
-        $contents_from_file = open_file($saved_filename);   
-        
-        // Checks if our checkbox is checked if true overrides the content of our file otherwise it adds the content of a file
-        //  to our original file
-        if (isset($_POST['checkboxfile'])){
-            $items = $contents_from_file;
-            save_file($filename, $items);
+          // create new instances of AddressDataStore when user uploads a file
+        $list_upload = new TodoList($saved_filename);
+        $upload_items= $list_upload->read();
 
-        }else{
-            $items = array_merge($items, $contents_from_file);
-            save_file($filename, $items);
+        if (isset($_POST['checkboxfile'])&& $_POST['checkboxfile'] == 'yes')
+        {
+            $items = $upload_items;
+            $list->write=$filename;
         }
+        else
+        {
+            $items = array_merge($items, $upload_items);
+            $list->filename = $filename;
+            $list->write($items);
+        }   
 
-    }
-}
-
-// check if the new item exists (new post) and is not empty
-if(isset($_POST['new_item']) && !empty($_POST['new_item'])){   
-  $new_item = $_POST['new_item'];     // store new item in item
-  array_push($items, $new_item);      // push the item into the items array
-  save_file($filename, $items);
-  header("Location: todo-list.php");
-}   
-
-// check for removal from list - process if exists
-if(isset($_GET['remove'])){   // check if the new item exists
-  $itemId = $_GET['remove'];     // store new item in item
-  unset($items[$itemId]);             //OR unset($items[$_GET['remove']]);
-  save_file($filename, $items);
-  header("Location: todo-list.php"); 
-  exit(0); // OR die(''); 
-}  
+     }else {
+      $error_msg = true;
+     } 
+}            
 
 
+ 
+    
+ 
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML!>
 <html>
 <head>
-    <title>Todo List</title>
+  <title>ToDo List</title>
 </head>
-    <body>
-      <h2> To Do List: </h2>
-        <? if (count($items) > 0) : ?>    
-              <ul>
-                   <?//add items from todo.txt to $items array
-                   foreach ($items as $key => $item) :    // iterates through an array of items and prints every element in the array ?>
-                       <li><?= htmlspecialchars(strip_tags($item)); ?> <a href ='?remove=<?=$key; ?>'>Remove</a></li>  
-                   <?endforeach;?>                                                                 
-              </ul>
-      <?else : ?>
-              <?= "You don't have any item"; ?>
-      <? endif; ?>        
-      <form method="POST" action = "">
-               <p>
-                <label for ="new_item">Item to add:</label>
-                <input id="new_item" name="new_item" type="text" autofocus = "autofocus" tabindex = "1" placeholder = "Enter the item to add">
-                <br><button type="add">Add Item</button>
-              </p>
-      </form>
-
-           <?if  ($error_msg == true) :
-                 echo "<p>You can't upload that file, we can only process .txt files</p>";
-            endif; ?>
+<body>
+      <h1>ToDo List</h1>
+      <? if (count($list->items) > 0 ): ?>
+        <ul>
+          <? foreach ($items as $key => $item): ?>
+            <li><?= $item; ?> | <a href='/todo-list.php?remove=<?= $key; ?>' name='remove' id='remove'>remove</a></li>
+          <? endforeach; ?>
+        </ul>
+      <? else: ?>
+        <p>You have 0 todo items.</p>
+      <? endif; ?>
+      <h2>Add new item to list</h2>
+          <form method="POST" action="">
+            <p>
+              <label for="newitem">Item to add:</label>
+              <input id="newitem" name="newitem" type="text" autofocus='autofocus' placeholder="Enter new TODO item">
+            </p>
+            <p>
+              <input type="submit" value="Add Item">
+            </p>
+          </form>
+            <? if  ($error_msg == true) :
+                  echo "<p>You can't upload that file, we can only process .txt files</p>";
+              endif; ?> 
       
       <h2>Upload File</h2>
       <form method="POST" enctype = "multipart/form-data" action = "todo-list.php">
@@ -124,15 +149,12 @@ if(isset($_GET['remove'])){   // check if the new item exists
                  <label for ="upload_file">File to add to list:</label>
                  <input id="upload_file" name="upload_file" type="file">
                  <br><input type="submit" value="Upload">
-                 <input type="checkbox" name="checkboxfile" value="override" checked> Do you want to override your file? 
+                 <input type="checkbox" name="checkboxfile" value="true" checked> Do you want to override your file? 
               </p>
-                   <? // Check if we saved a file
-                   if (isset($saved_filename)) :
-                       // If we did, show a link to the uploaded file
+                   <? //Check if we saved a file
+                   if (isset($saved_file)) :
+                       //If we did, show a link to the uploaded file
                       echo "<p>You can download your file <a href='/uploads/{$newfilename}'>here</a>.</p>";
-                   endif; ?>
-      </form>
-   </body>
+                   endif; ?> 
+</body>
 </html>
-
-

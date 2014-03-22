f947bd1b81b3836fe451a28786e690dfbfc032c9
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-03-22 16:15:09 -0500

    todoDB.html

--- public/todoDB.php
+++ public/todoDB.php
@@ -1,10 +1,10 @@
- <?php
- 
+<?php
+// Variables used for showing messages 
 $errorMessage = null;
 $successMessage = null;
 
 
-// connect to the db
+// Connect to the db
 $mysqli = new mysqli('127.0.0.1', 'codeup', 'password', 'codeup_test_db');
  
 // Check for errors
@@ -15,24 +15,25 @@ if ($mysqli->connect_errno)
 
 if (!empty($_POST))
 {
-    // check for new todo
+    // Check for new todo
     if(isset($_POST['todo']))
     {
     	if ($_POST['todo'] !="") 
     	{
-    		// limits item to 200 characters
+    		// Limits item to be 200 characters
     		$todo = substr($_POST['todo'], 0, 200);
 
-    		// add to db
+    		// Add item to db
     		$stmt = $mysqli->prepare("INSERT INTO todos (item) VALUES (?);");
     		$stmt->bind_param("s", $todo);
     		$stmt-> execute();
-
+            
+            // Show success message when item is added
     		$successMessage = "Todo item was added sucessfully.";
     	}
     	else
     	{
-    		// show error message
+    		// Show error message if user is trying to enter an empty item
     		$errorMessage = "Please input a todo item.";
     	}
     }
@@ -73,8 +74,75 @@ $nextPage = $currentPage < $maxPage ? $currentPage + 1 : null;
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 	<!-- Latest compiled and minified JavaScript -->
 	<!-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script> -->
+	<!--Google Fonts-->
+	<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+	<!--Font Awesome-->
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<style>
+	         h1{
+	         	
+	         	text-shadow: 5px 5px 5px #B1888D;
+			    color: #AB3244;
+			    text-decoration: underline;
+			    font-weight: bold;
+			    font-family: 'Lobster', Georgia, Times, serif;
+			    font-size: 50px;
+			    line-height: 50px;
+			    margin-left: 15px;
+	         }
+
+	         h2{
+			    color: #0D8CFF;
+			    text-decoration: underline;
+			    font-family: 'Lobster', Georgia, Times, serif;
+			    text-shadow: 5px 5px 5px #3D7078;
+			    font-size: 30px;
+			    
+	         }
+
+	         td{
+	         	 font-family: 'Cabin', sans-serif;
+	         	color : #009B9D;
+
+	         }
+
+	         body { background-image: url('../img/woven.png');  }
+            
+             footer{
+             	color: #C4361E;
+             }
+	         
+	</style>
 </head>
 <body>
+
+<div class="container2">
+      <!-- Static navbar -->
+      <div class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="todoDB.php">Todo Items App</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class ="#"><a href="national_parks.php">National Parks</a></li>	
+              <li class ="#"><a href="#">About Me</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+             <li><a href="http://www.linkedin.com/in/karinamontestrevino"><i class="fa fa-linkedin"></i></a></li> 
+             <li><a href="https://github.com/KarinaMontesTrevino"><i class="fa fa-github-alt"></i></a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </div> <!--/.navbar navbar-default-->
+ </div> <!-- /container -->	
 
 <div class = "container">
     
@@ -99,11 +167,11 @@ $nextPage = $currentPage < $maxPage ? $currentPage + 1 : null;
      
      <div class = "clearfix">
 		<? if ($prevPage != null): ?>
-	    <a href="?page=<?= $prevPage; ?>" class = "pull-left btn btn-default btn-sm">&lt;Previous</a>  
+	    <a href="?page=<?= $prevPage; ?>" class = "pull-left btn btn-primary btn-sm">&lt;&lt;Previous</a>  
 	    <?endif;?>
 
 	    <? if ($nextPage != null): ?>
-	      <a href="?page=<?= $nextPage; ?>" class = "pull-right btn btn-default btn-sm">Next&gt;</a>  
+	      <a href="?page=<?= $nextPage; ?>" class = "pull-right btn btn-primary btn-sm">Next&gt;&gt;</a>  
 	    <?endif;?>  
 	</div><!--.clearfix-->     
 
@@ -114,7 +182,7 @@ $nextPage = $currentPage < $maxPage ? $currentPage + 1 : null;
 	    <label class="sr-only" for="todo">Todo Item</label>
 	    <input id="todo" type="text" name = "todo" class="form-control"  placeholder="Enter todo item">
 	  </div>
-	  <button type="submit" class="btn btn-default">Add todo</button>
+	  <button type="submit" class="btn btn-success">Add todo</button>
 	</form>
 </div> <!--continer-->
 
@@ -136,8 +204,7 @@ $nextPage = $currentPage < $maxPage ? $currentPage + 1 : null;
 			form.submit();
 	    }
 	}
- 
 </script>
- 
+ <footer>&copy; Karina Montes-Trevino</footer>
 </body>
 </html>
